3111bfe78d49ded7409bf9a5b82eaa92d9170d08
siteations <studer@siteations.com>
2017-08-04 10:04:51 -0500

    heroku update

--- browser/react/non-db/panelsOther.js
+++ browser/react/non-db/panelsOther.js
@@ -45,10 +45,11 @@ export const panelsOther = {
 			'Tracy Cooper (Palladian churches)',
 			'Meredith Ray (Tarabotti)',
 			'Jim Akerman (cartography)',
+			'Liz Horodowich (various churches)',
 			'Deborah Howard and Laura Moretti (music in the churches)',
 			'Brian Sandberg (galley slaves)',
 			'Mara Wade (more on Germans)',
-			'Lia Markey (Dürer)',
+			'Lia Markey (Dürer)'
 			],
 		},
 	},
